831c1e64556556982eb14b32ea8b413604d0f080
Xiaoxue Ma <xm576@nyu.edu>
2021-03-09 11:30:22 -0800

    authen using username and password

--- .history/src/pages/Home_20210309111427.js
+++ .history/src/pages/Home_20210309111427.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import { useOktaAuth } from '@okta/okta-react';
-import { withOktaAuth } from '@okta/okta-react';
 
 const Home = () => {
   const { authState, oktaAuth } = useOktaAuth();
@@ -24,27 +23,6 @@ const Home = () => {
   );
 };
 export default Home;
-
-export default withOktaAuth(class Home extends Component {
-
-  render() {
-    if (this.props.authState.isPending) {
-      return <div>Loading...</div>;
-    }
-
-    const button = this.props.authState.isAuthenticated ?
-      <button onClick={() => {this.props.oktaAuth.signOut()}}>Logout</button> :
-      <button onClick={() => {this.props.history.push('/login')}}>Login</button>;
-
-    return (
-      <div>
-        <Link to='/'>Home</Link><br/>
-        <Link to='/protected'>Protected</Link><br/>
-        {button}
-      </div>
-    );
-  }
-});
 
 // import React from 'react';
 // import {
